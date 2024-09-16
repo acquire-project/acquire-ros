820e029c6263ccf569296fff611582f731778e8f
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-09-16 13:51:27 -0700

    Parameterized dimensions in zarr writer

--- acquire_zarr/README.md
+++ acquire_zarr/README.md
@@ -1,7 +1,7 @@
 # acquire_zarr
 This package wraps the acquire-driver-zarr standalone library into a ROS node that subscribes to an image topic and streams them to a Zarr file.  
 
-'As of this writing, this is still a work-in-progess, and many features which are supported by the library, are not yet supported here'''
+**As of this writing, this is still a work-in-progess, and many features which are supported by the library, are not yet supported here**
 
 ## Build instructions
 
--- acquire_zarr/src/zarr_writer_node.cpp
+++ acquire_zarr/src/zarr_writer_node.cpp
@@ -48,18 +48,45 @@ void ZarrWriterNode::settings_from_params()
 
   this->declare_parameter<std::string>("store_path", "out.zarr");
   auto store_path = this->get_parameter("store_path").as_string();
-  ZarrStreamSettings_set_store_path(this->zarr_stream_settings_, store_path.c_str(), store_path.size()+1);
+  ZarrStreamSettings_set_store(this->zarr_stream_settings_, store_path.c_str(), store_path.size()+1, nullptr);
 
   this->declare_parameter<int>("data_type", (int)ZarrDataType_uint8);
   auto data_type = this->get_parameter("data_type").as_int();
   ZarrStreamSettings_set_data_type(this->zarr_stream_settings_, (ZarrDataType)data_type);
 
 
-  ZarrStreamSettings_reserve_dimensions(zarr_stream_settings_, 3);
-  ZarrStreamSettings_set_dimension(zarr_stream_settings_, 0, "t", 2, ZarrDimensionType_Time, 0, 1, 1);
-  ZarrStreamSettings_set_dimension(zarr_stream_settings_, 1, "y", 2, ZarrDimensionType_Space, 480, 480, 1);
-  ZarrStreamSettings_set_dimension(zarr_stream_settings_, 2, "x", 2, ZarrDimensionType_Space, 640, 640, 1);
-  
+  this->declare_parameter("dimension_names", std::vector<std::string>{"t", "y", "x"});
+  auto dimension_names = this->get_parameter("dimension_names").as_string_array();
+  auto n_dimensions = dimension_names.size();
+
+  this->declare_parameter("dimension_types", std::vector<int>{(int)ZarrDimensionType_Time, (int)ZarrDimensionType_Space, (int)ZarrDimensionType_Space});
+  auto dimension_types = this->get_parameter("dimension_types").as_integer_array();
+
+  this->declare_parameter("dimension_sizes", std::vector<int>{2, 480, 640});
+  auto dimension_sizes = this->get_parameter("dimension_sizes").as_integer_array();
+
+  this->declare_parameter("dimension_chunk_px", std::vector<int>{1, 480, 640});
+  auto dimension_chunk_px_sizes = this->get_parameter("dimension_chunk_px").as_integer_array();
+
+  this->declare_parameter("dimension_shard_chunks", std::vector<int>{1, 1, 1});
+  auto dimension_shard_chunks = this->get_parameter("dimension_shard_chunks").as_integer_array();
+
+  ZarrStreamSettings_reserve_dimensions(zarr_stream_settings_, n_dimensions);
+
+
+  for(size_t i = 0; i < n_dimensions; i++)
+  {
+
+    ZarrDimensionProperties dim_props = {
+      dimension_names[i].c_str(),
+      dimension_names[i].size()+1,
+      (ZarrDimensionType)dimension_types[i],
+      (uint32_t)dimension_sizes[i],
+      (uint32_t)dimension_chunk_px_sizes[i],
+      (uint32_t)dimension_shard_chunks[i]
+    };
+    ZarrStreamSettings_set_dimension(zarr_stream_settings_, i, &dim_props);
+  }
 
   zarr_stream_ = ZarrStream_create(zarr_stream_settings_, ZarrVersion_2);
 
