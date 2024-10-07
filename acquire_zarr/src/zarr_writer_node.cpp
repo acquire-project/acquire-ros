821f456c3d19facfc205ccf0fbb3a537bdab5b3e
Justin Eskesen <jeskesen@chanzuckerberg.com>
2024-10-07 23:27:42 +0000

    Update zarr writer node source files to accomodated API changes.

--- acquire_zarr/src/zarr_writer_node.cpp
+++ acquire_zarr/src/zarr_writer_node.cpp
@@ -8,7 +8,6 @@
 #include "sensor_msgs/msg/image.hpp"
 
 #include "acquire_zarr/zarr_writer_node.hpp"
-#include "zarr.h"
 
 namespace acquire_zarr
 {
@@ -35,31 +34,30 @@ namespace acquire_zarr
     subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
       "image_raw", 10, callback, options);
 
-
   }
 
   ZarrWriterNode::~ZarrWriterNode()
   {
-    ZarrStreamSettings_destroy(zarr_stream_settings_);
+    ZarrStreamSettings_destroy_dimension_array(&zarr_stream_settings_);
   }
 
   void ZarrWriterNode::settings_from_params()
   {
 
-    zarr_stream_settings_ = ZarrStreamSettings_create();
+    zarr_stream_settings_.version = ZarrVersion_2;
 
     this->declare_parameter<std::string>("store_path", "out.zarr");
-    auto store_path = this->get_parameter("store_path").as_string();
-    ZarrStreamSettings_set_store(this->zarr_stream_settings_, store_path.c_str(), store_path.size()+1, nullptr);
+    store_path_ = this->get_parameter("store_path").as_string();
+    zarr_stream_settings_.store_path = store_path_.c_str();
 
     this->declare_parameter<int>("data_type", (int)ZarrDataType_uint8);
     auto data_type = this->get_parameter("data_type").as_int();
-    ZarrStreamSettings_set_data_type(this->zarr_stream_settings_, (ZarrDataType)data_type);
+    this->zarr_stream_settings_.data_type = (ZarrDataType)data_type;
 
 
     this->declare_parameter("dimension_names", std::vector<std::string>{"t", "y", "x"});
-    auto dimension_names = this->get_parameter("dimension_names").as_string_array();
-    auto n_dimensions = dimension_names.size();
+    dimension_names_ = this->get_parameter("dimension_names").as_string_array();
+    auto n_dimensions = dimension_names_.size();
 
     this->declare_parameter("dimension_types", std::vector<int>{(int)ZarrDimensionType_Time, (int)ZarrDimensionType_Space, (int)ZarrDimensionType_Space});
     auto dimension_types = this->get_parameter("dimension_types").as_integer_array();
@@ -73,24 +71,20 @@ namespace acquire_zarr
     this->declare_parameter("dimension_shard_chunks", std::vector<int>{1, 1, 1});
     auto dimension_shard_chunks = this->get_parameter("dimension_shard_chunks").as_integer_array();
 
-    ZarrStreamSettings_reserve_dimensions(zarr_stream_settings_, n_dimensions);
-
-
+    ZarrStreamSettings_create_dimension_array(&zarr_stream_settings_, n_dimensions);
     for(size_t i = 0; i < n_dimensions; i++)
     {
 
-      ZarrDimensionProperties dim_props = {
-        dimension_names[i].c_str(),
-        dimension_names[i].size()+1,
+      zarr_stream_settings_.dimensions[i] = {
+        dimension_names_[i].c_str(),
         (ZarrDimensionType)dimension_types[i],
         (uint32_t)dimension_sizes[i],
         (uint32_t)dimension_chunk_px_sizes[i],
         (uint32_t)dimension_shard_chunks[i]
       };
-      ZarrStreamSettings_set_dimension(zarr_stream_settings_, i, &dim_props);
     }
 
-    zarr_stream_ = ZarrStream_create(zarr_stream_settings_, ZarrVersion_2);
+    zarr_stream_ = ZarrStream_create(&zarr_stream_settings_);
 
   }
 
