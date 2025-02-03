f78daf415a8ee708acec230b69d188f40e4c64de
Justin Eskesen <jeskesen@chanzuckerberg.com>
2025-02-03 09:42:10 -0800

    added actual zarr comparison (not yet passing)

--- acquire_zarr/test/test_zarr_writer_node_launch.py
+++ acquire_zarr/test/test_zarr_writer_node_launch.py
@@ -14,19 +14,20 @@
 
 from pathlib import Path
 import sys
-from threading import Event
 from threading import Thread
+import numpy as np
+import zarr
+import pytest
+import time
 
 import launch
 import launch_pytest
 import launch_ros
 
-import pytest
-
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from sensor_msgs.msg import Image
 
 
 @launch_pytest.fixture
@@ -36,45 +37,66 @@ def generate_test_description():
     return launch.LaunchDescription([
 
         launch_ros.actions.Node(
-            package='demo_nodes_py',
-            executable="talker",
-            additional_env={'PYTHONUNBUFFERED': '1'},
-            name='demo_node_1',
+            package='acquire_zarr',
+            executable="image_zarr_writer_node",
             output='screen',
         ),
     ])
 
 
 @pytest.mark.launch(fixture=generate_test_description)
-def test_check_if_msgs_published():
+def test_data():
     rclpy.init()
     try:
-        node = MakeTestNode('test_node')
-        node.start_subscriber()
-        msgs_received_flag = node.msg_event_object.wait(timeout=5.0)
-        assert msgs_received_flag, 'Did not receive msgs !'
+        node = ZarrTestNode('test_node')
+        node.run_publisher()
+        
+        node.ros_spin_thread.join()
+        #node.validate_data()
+        assert True
+
     finally:
         rclpy.shutdown()
 
 
-class MakeTestNode(Node):
+class ZarrTestNode(Node):
 
     def __init__(self, name='test_node'):
         super().__init__(name)
-        self.msg_event_object = Event()
 
-    def start_subscriber(self):
-        # Create a subscriber
-        self.subscription = self.create_subscription(
-            String,
-            'chatter',
-            self.subscriber_callback,
-            10
-        )
-
+        self.zarr_shape = (10, 10, 480, 640)
+        self.data = np.random.randint(0, 255, self.zarr_shape)
+        
+        self.pub_ = self.create_publisher(Image, self.get_name()+'/image_raw', 10)
+        self.timer_ = self.create_timer(0.1, self.publish_msg)
+        
+        self.image_msg_ = Image()
+        self.image_msg_.header.frame_id = 'camera_frame'
+        self.image_msg_.height = 480
+        self.image_msg_.width = 640
+        self.image_msg_.encoding = 'mono8'
+        self.image_msg_.is_bigendian = 0
+        self.image_msg_.step = 640
+        
+    def run_publisher(self):
         # Add a spin thread
         self.ros_spin_thread = Thread(target=lambda node: rclpy.spin(node), args=(self,))
         self.ros_spin_thread.start()
-
-    def subscriber_callback(self, data):
-        self.msg_event_object.set()
+        
+        for i in range(self.zarr_shape[0]):
+            for j in range(self.zarr_shape[1]):
+                self.image_msg_.header.stamp = self.get_clock().now().to_msg()
+                self.image_msg_.data = self.data[i, j].tobytes()
+                self.pub_.publish(self.image_msg_)
+                self.get_logger().info('Publishing image %d %d', i, j)
+                rclpy.spin_once(self)
+                time.sleep(0.01)
+                
+        self.ros_spin_thread.join()
+        
+    def validate_data(self):
+        zarr_file = zarr.open('test.zarr', mode='r')
+        assert np.array_equal(zarr_file['image'], self.data)
+        zarr_file.close()
+        Path('test.zarr').unlink()
+        self.get_logger().info('Zarr file deleted')
